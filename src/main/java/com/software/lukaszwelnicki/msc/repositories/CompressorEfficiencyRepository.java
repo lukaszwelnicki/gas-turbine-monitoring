47f51b75fd0bc242a7cc68d797b73b5d990412e7
Łukasz Wełnicki <lukasz.welnicki@gmail.com>
2018-05-21 17:43:11 +0200

    Cleanup

--- src/main/java/com/software/lukaszwelnicki/msc/repositories/CompressorEfficiencyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.software.lukaszwelnicki.msc.repositories;
-
-import com.software.lukaszwelnicki.msc.model.CompressorEfficiency;
-import com.software.lukaszwelnicki.msc.model.Measurement;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface CompressorEfficiencyRepository<T extends CompressorEfficiency> extends MeasurementRepository<T> {
-
-}

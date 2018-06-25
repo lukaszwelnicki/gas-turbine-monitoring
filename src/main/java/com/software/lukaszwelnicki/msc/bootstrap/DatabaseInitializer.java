accc9afe2a8653883e9a2e36108e6e68a937ce4f
Łukasz Wełnicki <lukasz.welnicki@gmail.com>
2018-06-25 21:41:06 +0200

    Added logging functionality to DatabaseInitializer

--- src/main/java/com/software/lukaszwelnicki/msc/bootstrap/DatabaseInitializer.java
+++ src/main/java/com/software/lukaszwelnicki/msc/bootstrap/DatabaseInitializer.java
@@ -3,6 +3,7 @@ package com.software.lukaszwelnicki.msc.bootstrap;
 import com.software.lukaszwelnicki.msc.generator.DataGenerator;
 import com.software.lukaszwelnicki.msc.model.*;
 import com.software.lukaszwelnicki.msc.repositories.MeasurementRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Component
 public class DatabaseInitializer implements CommandLineRunner {
 
@@ -20,6 +22,7 @@ public class DatabaseInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
         measurementRepository.deleteAll();
         bootstrapDB(LocalDateTime.now().minusMonths(3), LocalDateTime.now(), 15);
+        log.info("Finished bootstrap. Number of recorts: " + measurementRepository.count().block());
     }
 
     private void bootstrapDB(LocalDateTime start, LocalDateTime end, int secondsBetweenRecords) {

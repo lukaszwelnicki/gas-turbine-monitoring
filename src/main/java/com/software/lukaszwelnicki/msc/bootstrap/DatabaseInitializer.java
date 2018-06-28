fe8447cbe5aca7e26278f35b6586fc0070f54901
Łukasz Wełnicki <lukasz.welnicki@gmail.com>
2018-06-28 23:08:04 +0200

    Imports optimization

--- src/main/java/com/software/lukaszwelnicki/msc/bootstrap/DatabaseInitializer.java
+++ src/main/java/com/software/lukaszwelnicki/msc/bootstrap/DatabaseInitializer.java
@@ -7,9 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component

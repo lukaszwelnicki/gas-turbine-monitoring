fda04e40fe1fd70131ce26090781a57b4e448fdb
welnil <lukasz.welnicki@vig.pl>
2018-08-21 11:30:49 +0200

    Revert "Records generation optimization with parallel stream"
    This reverts commit d246f3a

--- src/main/java/com/software/lukaszwelnicki/msc/generators/DataGenerator.java
+++ src/main/java/com/software/lukaszwelnicki/msc/generators/DataGenerator.java
@@ -4,12 +4,10 @@ import com.software.lukaszwelnicki.msc.measurements.documents.Measurement;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 
-import java.math.BigInteger;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 @EqualsAndHashCode
@@ -17,19 +15,15 @@ public class DataGenerator<T extends Measurement> {
 
     private final T measurement;
 
-    @SuppressWarnings("unchecked")
     public List<T> generateRecordsInBetweenDates(LocalDateTime start, LocalDateTime end, int secondsBetweenReadings) {
-        final BigInteger sampling = BigInteger.valueOf(secondsBetweenReadings);
-        int numberOfRecords = getNumberOfRecords(start, end, secondsBetweenReadings);
-        List<T> records = new ArrayList<>(numberOfRecords);
-        IntStream.range(0, numberOfRecords)
-                .parallel()
-                .mapToObj(BigInteger::valueOf)
-                .forEach(i -> {
-                    T record = (T) measurement.random();
-                    record.setCreatedDate(start.plusSeconds(i.multiply(sampling).longValue()));
-                    records.add(i.intValue(), record);
-                });
+        long numberOfRecords = getNumberOfRecords(start, end, secondsBetweenReadings);
+        List<T> records = new ArrayList<>();
+        for (int i = 0; i < numberOfRecords; i++) {
+            @SuppressWarnings("unchecked")
+            T record = (T) measurement.random();
+            record.setCreatedDate(start.plusSeconds(secondsBetweenReadings * i));
+            records.add(record);
+        }
         return records;
     }
 
@@ -40,21 +34,21 @@ public class DataGenerator<T extends Measurement> {
         Returning just one record did not work correctly.
     */
     @SuppressWarnings("unchecked")
-    public List<T> generateRandomRecordAsList() {
+    public List<T> generateRandomRecord() {
         List<T> records = new ArrayList<>();
         T record = (T) measurement.random();
         records.add(record);
         return records;
     }
 
-    final int getNumberOfRecords(LocalDateTime start, LocalDateTime end, int secondsBetweenReadings) {
+    final long getNumberOfRecords(LocalDateTime start, LocalDateTime end, int secondsBetweenReadings) {
         if (secondsBetweenReadings == 0) {
             throw new IllegalArgumentException("Seconds between readings must not be equal to zero");
         }
         if (end.isBefore(start) || end.isEqual(start)) {
             throw new IllegalArgumentException("End time should be after start time.");
         }
-        return (int) (Duration.between(start, end).getSeconds() / secondsBetweenReadings);
+        return Duration.between(start, end).getSeconds() / secondsBetweenReadings;
     }
 
 

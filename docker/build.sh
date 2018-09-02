66670df34aa4ff0d24cb5aa0660386ded8061148
Łukasz Wełnicki <lukasz.welnicki@gmail.com>
2018-09-02 22:46:34 +0200

    Added CircleCI task to build and deploy docker image

--- docker/build.sh
+++ docker/build.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 cd ..
-./gradlew clean build
 cp api/build/libs/api-*.jar docker/api.jar
 cd docker
 docker build . -t gas-turbine-monitoring:latest
 docker tag gas-turbine-monitoring:latest lukaszwelnicki/gas-turbine-monitoring:latest
-docker login
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 docker push lukaszwelnicki/gas-turbine-monitoring:latest
 rm api.jar
